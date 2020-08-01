a2918ca41e51233fccd8747016d782741d77c32e
suriya <suriyaindev@gmail.com>
2020-08-01 19:12:47 +0530

    Comment policy page redirect to youtube community guidliness page.

--- comment-policy.php
+++ comment-policy.php
@@ -1,5 +1,3 @@
-
-Our Community Guidelines follows  what's allowed and not allowed on YouTube, and apply to all types of content on our platform, including comments, links and other social feeds. Please find the below links for detailed youtube community guidelines
-
-
-<a href="https://www.youtube.com/howyoutubeworks/policies/community-guidelines/">Youtube comment policy</a>
+<script>
+window.location = "https://www.youtube.com/howyoutubeworks/policies/community-guidelines/";
+</script>
