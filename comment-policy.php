329d44a1bb60b0a6ba0fa73b5a9d7a6e8e14c47b
suriya <suriyaindev@gmail.com>
2020-08-01 19:01:11 +0530

    Community Guidelines

---
+++ comment-policy.php
@@ -0,0 +1,5 @@
+
+Our Community Guidelines follows  what's allowed and not allowed on YouTube, and apply to all types of content on our platform, including comments, links and other social feeds. Please find the below links for detailed youtube community guidelines
+
+
+<a href="https://www.youtube.com/howyoutubeworks/policies/community-guidelines/">Youtube comment policy</a>
